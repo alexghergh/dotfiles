064814fc95210415d6f563f6cd5967046bb65e07
Alexandru Gherghescu <gherghescu_alex1@yahoo.ro>
2023-06-04 01:50:11 +0300

    Initial work on updating signify

--- vim/.config/nvim/after/plugin/signify.lua
+++ vim/.config/nvim/after/plugin/signify.lua
@@ -1,34 +1,60 @@
--- due to how the column offset is calculated for the
--- hunk preview, a line needed to be commented in the
--- source code so the hunk preview is aligned to the
--- rest of the text
--- file changed: vim-signify/autoload/sy/util.vim - L201
+-- TODO
+-- play with foldcolumn a bit as well, see what happens in files that don't have
+-- folds, and see what happens in files that have over 999 lines
+-- due to how the column offset is calculated for the hunk preview, a line
+-- needed to be commented in the source code so the hunk preview is aligned to
+-- the rest of the text
+-- file changed: vim-signify/autoload/sy/util.vim - L222
 
--- show hunk diff preview window
-vim.api.nvim_set_keymap('n', "<Leader>hs", "<Cmd>SignifyHunkDiff<CR>", { noremap = true })
+-- see :h signify
 
--- hunk undo (similar to git checkout)
-vim.api.nvim_set_keymap('n', "<Leader>hu", "<Cmd>SignifyHunkUndo<CR>", { noremap = true })
+-- toggle line highlighting (mnemonic Hunk/Highlight Toggle)
+vim.keymap.set('n', '<Leader>ht', '<Cmd>SignifyToggleHighlight<Cr>')
 
--- go to next/previous hunks
-vim.api.nvim_set_keymap('n', "]h", "<Plug>(signify-next-hunk)", {})
-vim.api.nvim_set_keymap('n', "[h", "<Plug>(signify-prev-hunk)", {})
+-- open new tab in diff-mode (mnemonic Hunk Open)
+vim.keymap.set('n', '<Leader>ho', '<Cmd>SignifyDiff<Cr>')
 
--- go to first/last hunks
-vim.api.nvim_set_keymap('n', "]H", "9999]h", {})
-vim.api.nvim_set_keymap('n', "[H", "9999[h", {})
+-- open new tab only with changes unfolded (mnemonic Hunk Fold)
+vim.keymap.set('n', '<Leader>hf', '<Cmd>SignifyFold<Cr>')
 
--- show the current hunk number out of total hunks
-vim.cmd[[
-autocmd User SignifyHunk call ShowCurrentHunk()
+-- show diff in floating window (mnemonic Hunk Show)
+vim.keymap.set('n', '<Leader>hs', '<Cmd>SignifyHunkDiff<Cr>')
 
-function! ShowCurrentHunk() abort
-    let h = sy#util#get_hunk_stats()
-    if !empty(h)
-        echo printf('[Hunk %d/%d]', h.current_hunk, h.total_hunks)
-    endif
-endfunction
-]]
+-- undo the diff change on the line (mnemonic Hunk Undo)
+vim.keymap.set('n', '<Leader>hu', '<Cmd>SignifyHunkUndo<Cr>')
+
+-- go to next/previous Hunks
+vim.keymap.set('n', ']h', '<Plug>(signify-next-hunk)')
+vim.keymap.set('n', '[h', '<Plug>(signify-prev-hunk)')
+
+-- go to first/last Hunks
+vim.keymap.set('n', ']H', '9999]h', { remap = true })
+vim.keymap.set('n', '[H', '9999[h', { remap = true })
+
+-- hunk text objects
+vim.keymap.set('o', 'ih', '<Plug>(signify-motion-inner-pending)')
+vim.keymap.set('x', 'ih', '<Plug>(signify-motion-inner-visual)')
+vim.keymap.set('o', 'ah', '<Plug>(signify-motion-outer-pending)')
+vim.keymap.set('x', 'ah', '<Plug>(signify-motion-outer-visual)')
+
+-- show the current hunk number out of the total when jumping
+vim.api.nvim_create_autocmd('User', {
+    pattern = 'SignifyHunk',
+    callback = function(event)
+        h = vim.fn['sy#util#get_hunk_stats']()
+        if h ~= nil then
+            print(vim.fn.printf('[Hunk %d/%d]', h.current_hunk, h.total_hunks))
+        end
+    end,
+    group = vim.api.nvim_create_augroup('_user_group', { clear = false })
+})
+
+-- TODO add brighter colors, or change the background
+-- also, change the Diff highlight groups rather than the signify ones directly
+-- don't link these to the color scheme by default
+-- vim.cmd [[ highlight! SignifySignAdd NONE ]]
+-- vim.cmd [[ highlight! SignifySignChange NONE ]]
+-- vim.cmd [[ highlight! SignifySignDelete NONE ]]
 
 -- display the added/removed/modified lines in the statusline
 -- TODO seems to be bugged, completely overrides the default statusline
@@ -60,14 +86,3 @@ vim.cmd[[
 
 " set statusline=%!MyStatusline()
 ]]
-
--- hunk text objects
-vim.api.nvim_set_keymap('o', "ih", "<Plug>(signify-motion-inner-pending)", {})
-vim.api.nvim_set_keymap('x', "ih", "<Plug>(signify-motion-inner-visual)", {})
-vim.api.nvim_set_keymap('o', "ah", "<Plug>(signify-motion-outer-pending)", {})
-vim.api.nvim_set_keymap('x', "ah", "<Plug>(signify-motion-outer-visual)", {})
-
--- don't link these to the color scheme by default
-vim.cmd [[ highlight! SignifySignAdd NONE ]]
-vim.cmd [[ highlight! SignifySignChange NONE ]]
-vim.cmd [[ highlight! SignifySignDelete NONE ]]
