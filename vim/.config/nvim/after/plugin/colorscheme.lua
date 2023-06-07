a0da5229baca63b9c85e53d2f0045bd20efa850d
Alexandru Gherghescu <gherghescu_alex1@yahoo.ro>
2023-06-07 20:11:37 +0300

    Remove 'bold' from colorscheme

--- vim/.config/nvim/after/plugin/colorscheme.lua
+++ vim/.config/nvim/after/plugin/colorscheme.lua
@@ -16,14 +16,14 @@ if pcall(vim.cmd.colorscheme, 'melange') then
     vim.cmd('highlight StatusLine guibg=#282c34')
 
     -- set up statusline colors (matching tmux colorscheme)
-    vim.cmd('highlight StatusLineColor1 guifg=#3d4451 guibg=#8ab977 gui=bold')
-    vim.cmd('highlight StatusLineColor2 guifg=#a9a9af guibg=#3e4452 gui=bold')
+    vim.cmd('highlight StatusLineColor1 guifg=#3d4451 guibg=#8ab977')
+    vim.cmd('highlight StatusLineColor2 guifg=#a9a9af guibg=#3e4452')
     vim.cmd('highlight StatusLineColor3 guifg=#8ab977 guibg=#282c34')
 
     -- set up statusline diff stats (added, removed, modified)
     -- currently the background is hard-coded to color 2 above, since I couldn't
     -- find a way for the diffs to inherit the parent background
-    vim.cmd('highlight StatusLineDiffAdd guifg=#78997a guibg=#3e4452 gui=bold')
-    vim.cmd('highlight StatusLineDiffDelete guifg=#bd8183 guibg=#3e4452 gui=bold')
-    vim.cmd('highlight StatusLineDiffChange guifg=#b380b0 guibg=#3e4452 gui=bold')
+    vim.cmd('highlight StatusLineDiffAdd guifg=#78997a guibg=#3e4452')
+    vim.cmd('highlight StatusLineDiffDelete guifg=#bd8183 guibg=#3e4452')
+    vim.cmd('highlight StatusLineDiffChange guifg=#b380b0 guibg=#3e4452')
 end
