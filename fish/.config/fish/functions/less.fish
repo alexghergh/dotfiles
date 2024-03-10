1d7674134a48ac34acce0e6b6b86c555f508e78c
Alexandru Gherghescu <gherghescu_alex1@yahoo.ro>
2024-03-11 00:42:35 +0200

    [fish] Fix less function frontend
    Fix a bug where any file would be treated as archive, instead of
    differentiating normal files from archives. Now correctly works on
    normal files, as well as archives.

--- fish/.config/fish/functions/less.fish
+++ fish/.config/fish/functions/less.fish
@@ -2,15 +2,18 @@
 function less
     set -l i 1
     for arg in $argv
-        set argv[$i] (mktemp)
         switch $arg
             case '*.zip'
+                set argv[$i] (mktemp)
                 zip -sf $arg > $argv[$i]
             case '*.bz2'
+                set argv[$i] (mktemp)
                 bunzip2 -c $arg > $argv[$i]
             case '*.tar.gz'
+                set argv[$i] (mktemp)
                 tar --list -f $arg > $argv[$i]
             case '*.gz' '*.Z'
+                set argv[$i] (mktemp)
                 zcat $arg > $argv[$i]
         end
         set i (math $i + 1)
