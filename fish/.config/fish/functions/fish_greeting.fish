33dc50900441abc24f146b67f41d416924a99bfe
Alexandru Gherghescu <gherghescu_alex1@yahoo.ro>
2024-03-10 17:14:13 +0200

    [fish] Shorter terminal greeting

--- fish/.config/fish/functions/fish_greeting.fish
+++ fish/.config/fish/functions/fish_greeting.fish
@@ -1,11 +1,11 @@
 function fish_greeting
     # TODO colors for these too
     # TODO maybe a nice logo?
-    random choice \
-        "Hello trouble!" \
-        "What's cooking today?" \
-        "'aven't seen you here in a while, eh?" \
-        "こんにちは, motherfucker!"
+    set -l greeting (random choice \
+                        "Hello trouble!" \
+                        "What's cooking today?" \
+                        "'aven't seen you here in a while, eh?" \
+                        "こんにちは, motherfucker!")
 
     set -l is_login (status is-login
                         and echo "login"
@@ -15,6 +15,5 @@ function fish_greeting
                         and echo "interactive"
                         or echo "non-interactive")
 
-    echo -s "(You are currently running in a $is_login $is_interactive shell)"
-    echo -s "(The current time is: $(date +%X))"
+    echo -s "$greeting ($is_login $is_interactive) - $(date +%X)"
 end
