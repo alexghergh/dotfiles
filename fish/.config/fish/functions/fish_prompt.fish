b8eafc3bc5501bb2823aebfd4fbc264559a59c31
Alexandru Gherghescu <gherghescu_alex1@yahoo.ro>
2024-03-22 20:05:38 +0200

    [fish] Proper colorscheme for prompt
    This one actually borrows colors from fish's "system" colorscheme, and
    therefore adapts to how you choose a different colorscheme with
    "fish_config theme choose", so it's better for the long run.

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,28 +1,40 @@
 function fish_prompt
     set -l local_status $status # needs to be first thing
+
+    # some colors (borrowed from fish's colors to match the scheme)
     set -l normal (set_color normal)
+    set -l jobs_present_color $fish_color_error
+    set -l jobs_absent_color $fish_color_command
+    set -l username_color $fish_color_param
+    set -l hostname_color $fish_color_keyword
+    set -l cwd_color $fish_color_redirection
+    set -l vcs_color $fish_color_quote
+    set -l arrow_color $fish_color_autosuggestion
+    set -l sign_color $fish_color_autosuggestion
+    set -l cmd_duration_color $fish_color_comment
 
     # jobs
     jobs -q
-    and set_color $fish_color_jobs_present
-    or set_color $fish_color_jobs_absent
+    and set_color $jobs_present_color
+    or set_color $jobs_absent_color
     echo -ns "(" (count (string split "\n" (jobs -p))) ") " $normal
 
     # username and host
-    echo -ns (prompt_login) $normal
+    set -l color_host
+    echo -ns (set_color $username_color) "$USER" $normal @ (set_color $hostname_color) (prompt_hostname) $normal
 
     # path
-    echo -ns ":" (set_color $fish_color_cwd) (prompt_pwd) $normal
+    echo -ns ":" (set_color $cwd_color) (prompt_pwd) $normal
 
     # vcs info
-    echo -ns (fish_vcs_prompt) $normal
+    echo -ns (set_color $vcs_color) (fish_vcs_prompt) $normal
 
     # execution time of last command
     if test $CMD_DURATION -gt 1000
         set -l seconds (math --scale=0 "$CMD_DURATION / 1000 % 60")
         set -l minutes (math --scale=0 "$CMD_DURATION / (1000 * 60) % 60")
         set -l hours   (math --scale=0 "$CMD_DURATION / (1000 * 60 * 60)")
-        echo -ns (set_color $fish_color_cmd_duration) (printf ' (')
+        echo -ns (set_color $cmd_duration_color) (printf ' (')
         test $hours   -gt 0; and echo -ns (printf "%dh " $hours)
         test $minutes -gt 0; and echo -ns (printf "%dm " $minutes)
         test $seconds -gt 0; and echo -ns (printf "%ds" $seconds)
@@ -31,13 +43,13 @@ function fish_prompt
 
     # prompt suffix (1)
     echo -e ""
-    echo -ns (set_color $fish_color_suffix) \U279C
+    echo -ns (set_color $arrow_color) \U279C $normal
 
     # error code
     if [ $local_status -ne 0 ]
-        echo -ns (set_color red) " $local_status"
+        echo -ns (set_color red) " $local_status" $normal
     end
 
     # prompt suffix (2)
-    echo -ns (set_color $fish_color_suffix) " \$ " $normal
+    echo -ns (set_color $sign_color) " \$ " $normal
 end
