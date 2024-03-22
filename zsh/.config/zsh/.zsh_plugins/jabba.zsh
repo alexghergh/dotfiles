d3d2814106192ccfa286848002ecbb560a0288fd
Alexandru Gherghescu <gherghescu_alex1@yahoo.ro>
2024-03-22 17:30:10 +0200

    [zsh] Remove zsh configs
    Transition to full fish use

--- zsh/.config/zsh/.zsh_plugins/jabba.zsh
+++
@@ -1,9 +0,0 @@
-return 0
-
-## jabba (java version manager) setup
-
-## TODO
-if command -v jabba 1>/dev/null 2>&1; then
-    export JABBA_HOME="$HOME/.jabba"
-    [ -s "$JABBA_HOME/jabba.sh" ] && source "$JABBA_HOME/jabba.sh"
-fi
