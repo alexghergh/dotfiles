8d8fb94ec71ece8116b9704b26f537d9dd52911d
Alexandru Gherghescu <gherghescu_alex1@yahoo.ro>
2024-03-22 23:49:19 +0200

    [wezterm] More goodies
    Move some more keybinds from tmux over to wezterm.
    
    Launcher menu items.
    
    Other improved keybinds.

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -8,28 +8,56 @@ config.color_scheme = 'Afterglow'
 config.font = wezterm.font_with_fallback({
     'JetBrains Mono',
     'Fira Code',
-    'Hack'
+    'Hack',
 })
 config.warn_about_missing_glyphs = false
 
 config.inactive_pane_hsb = {
     saturation = 0.9,
-    brightness = 0.7,
+    brightness = 0.6,
 }
 
 config.window_background_opacity = 0.95
 
 config.default_prog = { '/usr/bin/fish', }
+config.set_environment_variables = {
+    SHELL = '/usr/bin/fish',
+}
 
 config.launch_menu = {
     {
         args = { 'htop' },
     },
     {
-        label = 'neovim init.lua',
+        label = 'neovim - init.lua',
         args = { 'nvim', 'init.lua' },
         cwd = '/home/alex/.config/nvim/',
-    }
+    },
+    {
+        label = 'neovim - keymaps.lua',
+        args = { 'nvim', 'keymaps.lua' },
+        cwd = '/home/alex/.config/nvim/lua/core',
+    },
+    {
+        label = 'tmux - tmux.conf',
+        args = { 'nvim', 'tmux.conf' },
+        cwd = '/home/alex/.config/tmux/',
+    },
+    {
+        label = 'wezterm - wezterm.lua',
+        args = { 'nvim', 'wezterm.lua' },
+        cwd = '/home/alex/.config/wezterm/',
+    },
+    {
+        label = 'fish - conf.d/',
+        args = { 'nvim', 'conf.d/' },
+        cwd = '/home/alex/.config/fish/',
+    },
+    {
+        label = 'fish - abbr.fish',
+        args = { 'nvim', 'conf.d/abbr.fish' },
+        cwd = '/home/alex/.config/fish/',
+    },
 }
 
 -- IME
@@ -44,18 +72,123 @@ config.keys = {
         mods = 'LEADER|CTRL',
         action = action.SendKey({ key = 'a', mods = 'CTRL' })
     },
+
     -- split vertically
     {
-        key = 'v',
-        mods = 'LEADER',
+        key = '|',
+        mods = 'LEADER|SHIFT',
         action = action.SplitHorizontal({ domain = 'CurrentPaneDomain' })
     },
-    -- split vertically
+    -- split horizontally
     {
-        key = 'h',
+        key = '-',
         mods = 'LEADER',
         action = action.SplitVertical({ domain = 'CurrentPaneDomain' })
     },
+
+    -- switch tab
+    {
+        key = 't',
+        mods = 'LEADER',
+        action = action.ActivateKeyTable({
+            name = 'activate_tab',
+            one_shot = false,
+        })
+    },
+    -- switch pane
+    {
+        key = 'h',
+        mods = 'META',
+        action = action.ActivatePaneDirection('Left'),
+    },
+    {
+        key = 'l',
+        mods = 'META',
+        action = action.ActivatePaneDirection('Right'),
+    },
+    {
+        key = 'j',
+        mods = 'META',
+        action = action.ActivatePaneDirection('Down'),
+    },
+    {
+        key = 'k',
+        mods = 'META',
+        action = action.ActivatePaneDirection('Up'),
+    },
+
+    -- resize pane
+    {
+        key = 'r',
+        mods = 'LEADER',
+        action = action.ActivateKeyTable({
+            name = 'resize_pane',
+            one_shot = false,
+        }),
+    },
+
+    -- show launcher
+    {
+        key = 's',
+        mods = 'LEADER',
+        action = action.ShowLauncher,
+    },
+
+    -- resize font
+    {
+        key = 'f',
+        mods = 'LEADER',
+        action = action.ActivateKeyTable({
+            name = 'resize_font',
+            one_shot = false,
+        }),
+    },
 }
+
+config.key_tables = {
+    activate_tab = {
+        { key = 'h', action = action.ActivateTabRelative(-1) },
+        { key = 'l', action = action.ActivateTabRelative( 1) },
+
+        { key = 'Escape', action = 'PopKeyTable' },
+    },
+    resize_pane = {
+        { key = 'LeftArrow', action = action.AdjustPaneSize({ 'Left', 1 }) },
+        { key = 'h', action = action.AdjustPaneSize({ 'Left', 1 }) },
+
+        { key = 'RightArrow', action = action.AdjustPaneSize({ 'Right', 1 }) },
+        { key = 'l', action = action.AdjustPaneSize({ 'Right', 1 }) },
+
+        { key = 'DownArrow', action = action.AdjustPaneSize({ 'Down', 1 }) },
+        { key = 'j', action = action.AdjustPaneSize({ 'Down', 1 }) },
+
+        { key = 'UpArrow', action = action.AdjustPaneSize({ 'Up', 1 }) },
+        { key = 'k', action = action.AdjustPaneSize({ 'Up', 1 }) },
+
+        { key = 'Escape', action = 'PopKeyTable' },
+    },
+    resize_font = {
+        { key = 'j', action = action.IncreaseFontSize },
+        { key = '=', action = action.IncreaseFontSize },
+        { key = '+', mods='SHIFT', action = action.IncreaseFontSize },
+
+        { key = 'k', action = action.DecreaseFontSize },
+        { key = '-', action = action.DecreaseFontSize },
+        { key = '_', mods='SHIFT', action = action.DecreaseFontSize },
+
+        { key = 'r', action = action.ResetFontSize },
+
+        { key = 'Escape', action = 'PopKeyTable' },
+    }
+}
+
+-- display activation key table
+wezterm.on('update-right-status', function(window, _)
+    local name = window:active_key_table()
+    if name then
+        name = 'KEY TABLE: ' .. name
+    end
+    window:set_right_status(name or '')
+end)
 
 return config
