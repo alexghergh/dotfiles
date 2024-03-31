5716faaf8220e936b8a3f16c80f3ed6f720eeb2f
Alexandru Gherghescu <gherghescu_alex1@yahoo.ro>
2024-03-31 21:20:39 +0300

    [wez] Re-enable warn about missing glyphs config
    This config should really be enabled, as it shows problems with missing
    fonts. It was originally enabled because some Unicode emojis weren't
    displayed in wezterm, however it turned out to indeed be a missing font,
    easily installed then with the system's package manager.

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -13,7 +13,6 @@ config.font = wezterm.font_with_fallback({
 })
 config.font_size = 12
 config.adjust_window_size_when_changing_font_size = false
-config.warn_about_missing_glyphs = false
 
 config.window_padding = {
     left = 5,
