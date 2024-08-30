7f6be4837d09f9cbbc9ab15907f7d6d377598cc6
Alexandru Gherghescu <gherghescu_alex1@yahoo.ro>
2024-08-30 16:07:27 +0300

    [fish] Don't use --total-size as default in eza
    Since it's quite common to ls in dirs such as home or /, the default
    takes way too much. Keep --total-size as an option on 'll' when it's
    really needed.

--- fish/.config/fish/functions/l.fish
+++ fish/.config/fish/functions/l.fish
@@ -2,20 +2,18 @@ function l
     # if eza is present, use it as opposed to built-in ls
     # see https://github.com/eza-community/eza
     if type -q eza
-        eza -la --icons=auto --color=auto --group --links --git --git-repos --time-style=relative --total-size --header $argv
+        eza -la --icons=auto --color=auto --group --links --git --git-repos --time-style=relative --header $argv
     else
         ls -lah --color=tty $argv
     end
 end
 
-# this should be a faster, more stripped-down version of the above; should
-# probably only be needed if the directory is big, and --total-size takes too
-# much to load
+# slower, but displays total size of dirs as well
 function ll
     # if eza is present, use it as opposed to built-in ls
     # see https://github.com/eza-community/eza
     if type -q eza
-        eza -la --icons=auto --color=auto --group --links --time-style=relative $argv
+        eza -la --icons=auto --color=auto --group --links --total-size --time-style=relative $argv
     else
         ls -lah --color=tty $argv
     end
