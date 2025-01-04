6ad0ae8a4f081b0b4d38b63a6d863700e54f5106
Alexandru Gherghescu <gherghescu_alex1@yahoo.ro>
2025-01-04 03:02:04 +0200

    [vim] Refactor status line component
    It is much easier now to add components anywhere in the status line,
    without the need to renumber.

--- vim/.config/nvim/lua/modules/galaxyline.lua
+++ vim/.config/nvim/lua/modules/galaxyline.lua
@@ -3,34 +3,15 @@
 local function sep(style)
     if style == 'arrow' then
         -- stylua: ignore
-        return {
-            LSIDE_FULL  = '',
-            LSIDE_EMPTY = '',
-            RSIDE_FULL  = '',
-            RSIDE_EMPTY = '',
-        }
+        return { L_FULL = '', L_EMPTY = '', R_FULL = '', R_EMPTY = '' }
     elseif style == 'gaps' then
-        -- stylua: ignore
-        return {
-            LSIDE_FULL  = '', -- this might not render properly in certain fonts
-            LSIDE_EMPTY = '\\',
-            RSIDE_FULL  = '', -- this might not render properly in certain fonts
-            RSIDE_EMPTY = '/',
-        }
+        return { L_FULL = '', L_EMPTY = '\\', R_FULL = '', R_EMPTY = '/' }
     else
-        -- stylua: ignore
-        return {
-            LSIDE_FULL  = '',
-            LSIDE_EMPTY = '|',
-            RSIDE_FULL  = '',
-            RSIDE_EMPTY = '|',
-        }
+        return { L_FULL = '', L_EMPTY = '|', R_FULL = '', R_EMPTY = '|' }
     end
 end
 
 return {
-
-    -- custom statusline
     {
         'nvimdev/galaxyline.nvim',
         opts = {
@@ -43,230 +24,214 @@ return {
             --
             -- left section
             --
-
-            -- empty space
-            gls.left[1] = {
-                EmptySpace1 = {
-                    -- stylua: ignore
-                    provider = function() return ' ' end,
-                    highlight = 'StatusLineColor1',
-                },
-            }
-
-            -- nvim mode
-            gls.left[2] = {
-                CurrentMode = {
-                    provider = function()
+            local left_statusline_section = {
+                {
+                    EmptySpace1 = {
                         -- stylua: ignore
-                        local modes_map = {
-                            n         = 'NORMAL',
-                            no        = 'O-PENDING',
-                            nov       = 'O-PENDING',
-                            noV       = 'O-PENDING',
-                            ['no\22'] = 'O-PENDING',
-                            niI       = 'NORMAL',
-                            niR       = 'NORMAL',
-                            niV       = 'NORMAL',
-                            nt        = 'NORMAL',
-                            ntT       = 'NORMAL',
-                            v         = 'VISUAL',
-                            vs        = 'VISUAL',
-                            V         = 'V-LINE',
-                            Vs        = 'V-LINE',
-                            ['\22']   = 'V-BLOCK',
-                            ['\22s']  = 'V-BLOCK',
-                            s         = 'SELECT',
-                            S         = 'S-LINE',
-                            ['\19']   = 'S-BLOCK',
-                            i         = 'INSERT',
-                            ic        = 'INSERT',
-                            ix        = 'INSERT',
-                            R         = 'REPLACE',
-                            Rc        = 'REPLACE',
-                            Rx        = 'REPLACE',
-                            Rv        = 'V-REPLACE',
-                            Rvc       = 'V-REPLACE',
-                            Rvx       = 'V-REPLACE',
-                            c         = 'COMMAND',
-                            cv        = 'EX',
-                            ce        = 'EX',
-                            r         = 'REPLACE',
-                            rm        = 'MORE',
-                            ['r?']    = 'CONFIRM',
-                            ['!']     = 'SHELL',
-                            t         = 'TERMINAL',
-                        }
-                        return modes_map[vim.fn.mode()] or 'None'
-                    end,
-                    highlight = 'StatusLineColor1',
+                        provider = function() return ' ' end,
+                        highlight = 'StatusLineColor1',
+                    },
                 },
-            }
-
-            -- empty space
-            gls.left[3] = {
-                EmptySpace2 = {
-                    -- stylua: ignore
-                    provider = function() return ' ' end,
-                    highlight = 'StatusLineColor1',
-                    separator = sep(opts.separator_style).LSIDE_FULL,
-                    separator_highlight = 'StatusLineSeparator12',
+                {
+                    CurrentMode = {
+                        provider = function()
+                            -- stylua: ignore
+                            local modes_map = {
+                                n         = 'NORMAL',
+                                no        = 'O-PENDING',
+                                nov       = 'O-PENDING',
+                                noV       = 'O-PENDING',
+                                ['no\22'] = 'O-PENDING',
+                                niI       = 'NORMAL',
+                                niR       = 'NORMAL',
+                                niV       = 'NORMAL',
+                                nt        = 'NORMAL',
+                                ntT       = 'NORMAL',
+                                v         = 'VISUAL',
+                                vs        = 'VISUAL',
+                                V         = 'V-LINE',
+                                Vs        = 'V-LINE',
+                                ['\22']   = 'V-BLOCK',
+                                ['\22s']  = 'V-BLOCK',
+                                s         = 'SELECT',
+                                S         = 'S-LINE',
+                                ['\19']   = 'S-BLOCK',
+                                i         = 'INSERT',
+                                ic        = 'INSERT',
+                                ix        = 'INSERT',
+                                R         = 'REPLACE',
+                                Rc        = 'REPLACE',
+                                Rx        = 'REPLACE',
+                                Rv        = 'V-REPLACE',
+                                Rvc       = 'V-REPLACE',
+                                Rvx       = 'V-REPLACE',
+                                c         = 'COMMAND',
+                                cv        = 'EX',
+                                ce        = 'EX',
+                                r         = 'REPLACE',
+                                rm        = 'MORE',
+                                ['r?']    = 'CONFIRM',
+                                ['!']     = 'SHELL',
+                                t         = 'TERMINAL',
+                            }
+                            return modes_map[vim.fn.mode()] or 'None'
+                        end,
+                        highlight = 'StatusLineColor1',
+                    },
                 },
-            }
-
-            -- git branch
-            gls.left[4] = {
-                GitBranch = {
-                    provider = 'GitBranch',
-                    condition = condition.check_git_workspace,
-                    icon = '  ',
-                    highlight = 'StatusLineColor2',
+                {
+                    EmptySpace2 = {
+                        -- stylua: ignore
+                        provider = function() return ' ' end,
+                        highlight = 'StatusLineColor1',
+                        separator = sep(opts.separator_style).L_FULL,
+                        separator_highlight = 'StatusLineSeparator12',
+                    },
                 },
-            }
-
-            -- empty space
-            gls.left[5] = {
-                EmptySpace3 = {
-                    -- stylua: ignore
-                    provider = function() return ' ' end,
-                    highlight = 'StatusLineColor2',
-                    separator = sep(opts.separator_style).LSIDE_FULL,
-                    separator_highlight = 'StatusLineSeparator23',
+                {
+                    GitBranch = {
+                        provider = 'GitBranch',
+                        condition = condition.check_git_workspace,
+                        icon = '  ',
+                        highlight = 'StatusLineColor2',
+                    },
                 },
-            }
-
-            -- diff add
-            gls.left[6] = {
-                DiffAdd = {
-                    provider = 'DiffAdd',
-                    condition = condition.check_git_workspace
-                        and condition.hide_in_width,
-                    icon = ' +',
-                    highlight = 'StatusLineDiffAdd',
+                {
+                    EmptySpace3 = {
+                        -- stylua: ignore
+                        provider = function() return ' ' end,
+                        highlight = 'StatusLineColor2',
+                        separator = sep(opts.separator_style).L_FULL,
+                        separator_highlight = 'StatusLineSeparator23',
+                    },
                 },
-            }
-
-            -- diff delete
-            gls.left[7] = {
-                DiffRemove = {
-                    provider = 'DiffRemove',
-                    condition = condition.check_git_workspace
-                        and condition.hide_in_width,
-                    icon = ' -',
-                    highlight = 'StatusLineDiffDelete',
+                {
+                    DiffAdd = {
+                        provider = 'DiffAdd',
+                        condition = condition.check_git_workspace
+                            and condition.hide_in_width,
+                        icon = ' +',
+                        highlight = 'StatusLineDiffAdd',
+                    },
                 },
-            }
-
-            -- diff modified
-            gls.left[8] = {
-                DiffModified = {
-                    provider = 'DiffModified',
-                    condition = condition.check_git_workspace
-                        and condition.hide_in_width,
-                    icon = ' ~',
-                    highlight = 'StatusLineDiffChange',
+                {
+                    DiffRemove = {
+                        provider = 'DiffRemove',
+                        condition = condition.check_git_workspace
+                            and condition.hide_in_width,
+                        icon = ' -',
+                        highlight = 'StatusLineDiffDelete',
+                    },
+                },
+                {
+                    DiffModified = {
+                        provider = 'DiffModified',
+                        condition = condition.check_git_workspace
+                            and condition.hide_in_width,
+                        icon = ' ~',
+                        highlight = 'StatusLineDiffChange',
+                    },
                 },
             }
 
             --
             -- middle section
             --
-
-            -- file name
-            gls.mid[1] = {
-                FilePath = {
-                    provider = 'FilePath',
-                    highlight = 'StatusLineColor3',
+            local middle_statusline_section = {
+                {
+                    FilePath = {
+                        provider = 'FilePath',
+                        highlight = 'StatusLineColor3',
+                    },
                 },
-            }
-
-            -- buffer number
-            gls.mid[2] = {
-                BufferNumber = {
-                    provider = 'BufferNumber',
-                    highlight = 'StatusLineColor3',
+                {
+                    BufferNumber = {
+                        provider = 'BufferNumber',
+                        highlight = 'StatusLineColor3',
+                    },
                 },
-            }
-
-            -- argument list
-            gls.mid[3] = {
-                ArgsList = {
-                    provider = function()
-                        if vim.fn.argc() > 1 then
-                            return '('
-                                .. vim.fn.argidx() + 1
-                                .. '/'
-                                .. vim.fn.argc()
-                                .. ')'
-                        end
-                    end,
-                    icon = ' ',
-                    highlight = 'StatusLineColor3',
+                {
+                    ArgsList = {
+                        provider = function()
+                            if vim.fn.argc() > 1 then
+                                return '('
+                                    .. vim.fn.argidx() + 1
+                                    .. '/'
+                                    .. vim.fn.argc()
+                                    .. ')'
+                            end
+                        end,
+                        icon = ' ',
+                        highlight = 'StatusLineColor3',
+                    },
                 },
             }
 
             --
             -- right section
             --
-
-            -- file encoding
-            gls.right[1] = {
-                FileEncoding = {
-                    provider = 'FileEncode',
-                    highlight = 'StatusLineColor3',
-                    separator = sep(opts.separator_style).RSIDE_EMPTY,
-                    separator_highlight = 'StatusLineSeparator23',
+            local right_statusline_section = {
+                {
+                    FileEncoding = {
+                        provider = 'FileEncode',
+                        highlight = 'StatusLineColor3',
+                        separator = sep(opts.separator_style).R_EMPTY,
+                        separator_highlight = 'StatusLineSeparator23',
+                    },
+                },
+                {
+                    EmptySpace4 = {
+                        -- stylua: ignore
+                        provider = function() return ' ' end,
+                        highlight = 'StatusLineColor3',
+                    },
+                },
+                {
+                    FileType = {
+                        provider = 'FileTypeName',
+                        highlight = 'StatusLineColor3',
+                        icon = ' ',
+                        separator = sep(opts.separator_style).R_EMPTY,
+                        separator_highlight = 'StatusLineSeparator23',
+                    },
+                },
+                {
+                    EmptySpace5 = {
+                        -- stylua: ignore
+                        provider = function() return ' ' end,
+                        highlight = 'StatusLineColor3',
+                    },
+                },
+                {
+                    RowAndColumn = {
+                        provider = 'LineColumn',
+                        highlight = 'StatusLineColor2',
+                        icon = ' ',
+                        separator = sep(opts.separator_style).R_FULL,
+                        separator_highlight = 'StatusLineSeparator23',
+                    },
+                },
+                {
+                    LinePercent = {
+                        provider = 'LinePercent',
+                        highlight = 'StatusLineColor2',
+                        separator = sep(opts.separator_style).R_EMPTY,
+                        separator_highlight = 'StatusLineColor2',
+                    },
                 },
             }
 
-            -- empty space
-            gls.right[2] = {
-                EmptySpace4 = {
-                    -- stylua: ignore
-                    provider = function() return ' ' end,
-                    highlight = 'StatusLineColor3',
-                },
-            }
+            for _, item in ipairs(left_statusline_section) do
+                table.insert(gls.left, item)
+            end
 
-            -- file type
-            gls.right[3] = {
-                FileType = {
-                    provider = 'FileTypeName',
-                    highlight = 'StatusLineColor3',
-                    icon = ' ',
-                    separator = sep(opts.separator_style).RSIDE_EMPTY,
-                    separator_highlight = 'StatusLineSeparator23',
-                },
-            }
+            for _, item in ipairs(middle_statusline_section) do
+                table.insert(gls.mid, item)
+            end
 
-            -- empty space
-            gls.right[4] = {
-                EmptySpace4 = {
-                    -- stylua: ignore
-                    provider = function() return ' ' end,
-                    highlight = 'StatusLineColor2',
-                },
-            }
-
-            -- row and column
-            gls.right[5] = {
-                LineColumn = {
-                    provider = 'LineColumn',
-                    highlight = 'StatusLineColor2',
-                    icon = ' ',
-                    separator = sep(opts.separator_style).RSIDE_FULL,
-                    separator_highlight = 'StatusLineSeparator23',
-                },
-            }
-
-            -- line percentage of file
-            gls.right[6] = {
-                LinePercent = {
-                    provider = 'LinePercent',
-                    highlight = 'StatusLineColor2',
-                    separator = sep(opts.separator_style).RSIDE_EMPTY,
-                    separator_highlight = 'StatusLineColor2',
-                },
-            }
+            for _, item in ipairs(right_statusline_section) do
+                table.insert(gls.right, item)
+            end
         end,
     },
 }
