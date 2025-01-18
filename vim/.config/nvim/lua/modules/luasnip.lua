0e23aee8d0f86f4585d0dc390d3fb81600f35ee6
Alexandru Gherghescu <gherghescu_alex1@yahoo.ro>
2025-01-19 00:46:17 +0200

    [vim] Work on snippets support

--- vim/.config/nvim/lua/modules/luasnip.lua
+++ vim/.config/nvim/lua/modules/luasnip.lua
@@ -3,23 +3,22 @@ return {
     -- snippets support
 
     -- see :h luasnip.txt
-    -- see lua/modules/lspconfig.lua
+    -- see lua/modules/nvim-cmp.lua
     {
         'L3MON4D3/LuaSnip',
+        tag = 'v2.*',
         run = 'make install_jsregexp',
         dependencies = {
             'honza/vim-snippets',
         },
-        config = function(_, opts)
+        config = function(_, _)
             -- snippet expand/jumps and change choice are mapped in
-            -- lua/modules/lspconfig.lua as Tab/S-Tab/Ctrl-e
+            -- lua/modules/nvim-cmp.lua as Tab/S-Tab/Ctrl-e
 
             -- load vim-snippets
             require('luasnip.loaders.from_snipmate').lazy_load()
         end,
-    }, --, tag = 'v2.*', run = 'make install_jsregexp' })
-
-    { 'honza/vim-snippets' },
+    },
 }
 
 -- vim: set tw=0 fo-=r ft=lua
--- vim/.config/nvim/lua/modules/nvim-cmp.lua
+++ vim/.config/nvim/lua/modules/nvim-cmp.lua
@@ -103,16 +103,30 @@ return {
                             fallback()
                         end
                     end, { 'i', 's' }),
-                    ['<CR>'] = cmp.mapping.confirm(),
+                    ['<CR>'] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            if luasnip.expandable() then
+                                luasnip.expand()
+                            else
+                                cmp.confirm({ select = true })
+                            end
+                        else
+                            fallback()
+                        end
+                    end),
                     ['<Tab>'] = cmp.mapping(function(fallback)
-                        if luasnip.expand_or_jumpable() then
-                            luasnip.expand_or_jump()
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif luasnip.locally_jumpable(1) then
+                            luasnip.jump(1)
                         else
                             fallback()
                         end
                     end, { 'i', 's' }),
                     ['<S-Tab>'] = cmp.mapping(function(fallback)
-                        if luasnip.jumpable(-1) then
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif luasnip.locally_jumpable(-1) then
                             luasnip.jump(-1)
                         else
                             fallback()
