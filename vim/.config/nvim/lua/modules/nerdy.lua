9ab19e6ff0a17d4790288309ae9cfa679fd91716
Alexandru Gherghescu <gherghescu_alex1@yahoo.ro>
2025-01-19 01:24:57 +0200

    [vim] Add glyphs and symbols to telescope

--- vim/.config/nvim/lua/modules/nerdy.lua
+++ vim/.config/nvim/lua/modules/nerdy.lua
@@ -1,13 +1,32 @@
 return {
 
-    -- fonts, glyphs, symbols
+    -- better UI
+    {
+        'stevearc/dressing.nvim',
+        opts = {},
+        event = { 'VeryLazy' },
+    },
+
+    -- glyphs, symbols
     {
         '2kabhishek/nerdy.nvim',
-        cmd = 'Nerdy',
         dependencies = {
             'stevearc/dressing.nvim',
             'nvim-telescope/telescope.nvim',
         },
+        config = function()
+            local telescope = require('telescope')
+
+            telescope.load_extension('nerdy')
+
+            -- GLyphs
+            vim.keymap.set(
+                'n',
+                '<Leader>gl',
+                telescope.extensions.nerdy.nerdy,
+                {}
+            )
+        end,
     },
 }
 
