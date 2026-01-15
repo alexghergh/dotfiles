0f580cff945a55ee252a5172693f163766543358
Alexandru Gherghescu <gherghescu_alex1@yahoo.ro>
2026-01-15 11:25:46 +0900

    [nvim] Add better markdown renderer
    Mainly used for the chat window for AI code companion.

---
+++ vim/.config/nvim/lua/modules/markdown.lua
@@ -0,0 +1,12 @@
+return {
+
+    -- better markdown rendering
+    {
+        'MeanderingProgrammer/render-markdown.nvim',
+        ft = { 'markdown', 'codecompanion' },
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter',
+        },
+        opts = {},
+    }
+}
