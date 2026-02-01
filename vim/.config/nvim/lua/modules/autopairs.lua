786fdaaae3460f3bdefadeac3fd41ea7be3ed8a4
Alexandru Gherghescu <gherghescu_alex1@yahoo.ro>
2026-02-02 00:27:17 +0900

    [nvim] Fix autopairs not working in vim.ui.input
    In snacks' vim.ui.input insert mode, <c-w> seems to be somehow bugged
    into calling the normal mode's <c-w> (i.e. window stuff). Exclude the
    filetype by default, as well as telescope prompts.

--- vim/.config/nvim/lua/modules/autopairs.lua
+++ vim/.config/nvim/lua/modules/autopairs.lua
@@ -6,6 +6,7 @@ return {
         'windwp/nvim-autopairs',
         event = 'InsertEnter',
         opts = {
+            disable_filetype = { 'TelescopePrompt', 'snacks_input' },
             map_c_h = true, -- auto-delete pair on backspace if possible
             map_c_w = true, -- auto-delete pair on <c-w> if possible
         },
