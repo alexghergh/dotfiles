6e8859fdf828f9fa78c0b7fd98bc2158ce53bd06
Alexandru Gherghescu <gherghescu_alex1@yahoo.ro>
2026-03-07 17:52:52 +0900

    [nvim] Cleaner insert-mode movements keymaps
    - Cleaner insert mode left / right movement keymaps through A-h/l
    - A-j/k now move the current line up / down, instead of moving just the
      cursor

--- vim/.config/nvim/lua/core/keymaps.lua
+++ vim/.config/nvim/lua/core/keymaps.lua
@@ -19,27 +19,46 @@
 vim.keymap.set({ 'n', 'x' }, 'j', "v:count ? 'j' : 'gj'", { expr = true })
 vim.keymap.set({ 'n', 'x' }, 'k', "v:count ? 'k' : 'gk'", { expr = true })
 
--- in insert mode, move cursor left/right/down/up using Alt key
-vim.keymap.set('i', '<A-h>', function()
-    local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-    vim.api.nvim_win_set_cursor(0, { row, col ~= 0 and col - 1 or col })
-end, { desc = 'Move left in insert mode' })
-vim.keymap.set('i', '<A-l>', function()
-    local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-    local line_len = #vim.api.nvim_get_current_line()
-    vim.api.nvim_win_set_cursor(0, { row, col ~= line_len and col + 1 or col })
-end, { desc = 'Move right in insert mode' })
+-- in insert mode, move cursor left/right
+vim.keymap.set('i', '<A-h>', '<Left>', { desc = 'Move cursor left in insert mode' })
+vim.keymap.set('i', '<A-l>', '<Right>', { desc = 'Move cursor right in insert mode' })
+
+-- in insert mode, move line up/down (similar to unimpaired's [e and ]e in normal mode)
 vim.keymap.set('i', '<A-j>', function()
     local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-    local buf_len = vim.api.nvim_buf_line_count(0)
-    vim.api.nvim_win_set_cursor(0, { row ~= buf_len and row + 1 or row, col })
-end, { desc = 'Move down in insert mode' })
+    local last = vim.api.nvim_buf_line_count(0)
+    -- stylua: ignore
+    if row >= last then return end
+
+    -- keep each line movement a separate undo position
+    local keys = vim.api.nvim_replace_termcodes('<C-g>u', true, false, true)
+    vim.api.nvim_feedkeys(keys, 'n', false)
+
+    local lines = vim.api.nvim_buf_get_lines(0, row - 1, row + 1, true)
+    -- stylua: ignore
+    if #lines < 2 then return end
+
+    vim.api.nvim_buf_set_lines(0, row - 1, row + 1, true, { lines[2], lines[1] })
+    vim.api.nvim_win_set_cursor(0, { row + 1, math.min(col, #lines[1]) })
+end, { desc = 'Move current line down in insert mode' })
 vim.keymap.set('i', '<A-k>', function()
     local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-    vim.api.nvim_win_set_cursor(0, { row ~= 1 and row - 1 or row, col })
-end, { desc = 'Move up in insert mode' })
+    -- stylua: ignore
+    if row == 1 then return end
 
--- make <C-w> and <C-u> undo friendly (see :h i_CTRL-G_u)
+    local lines = vim.api.nvim_buf_get_lines(0, row - 2, row, true)
+    -- stylua: ignore
+    if #lines < 2 then return end
+
+    -- keep each line movement a separate undo position
+    local keys = vim.api.nvim_replace_termcodes('<C-g>u', true, false, true)
+    vim.api.nvim_feedkeys(keys, 'n', false)
+
+    vim.api.nvim_buf_set_lines(0, row - 2, row, true, { lines[2], lines[1] })
+    vim.api.nvim_win_set_cursor(0, { row - 1, math.min(col, #lines[2]) })
+end, { desc = 'Move current line up in insert mode' })
+
+-- make <C-w> and <C-u> undo-friendly (see :h i_CTRL-G_u)
 vim.keymap.set('i', '<C-w>', '<C-g>u<C-w>')
 vim.keymap.set('i', '<C-u>', '<C-g>u<C-u>')
 
