5c1fd5a861a130d97b6eea13a8c21fe81c24e6a1
Alexandru Gherghescu <gherghescu_alex1@yahoo.ro>
2026-04-12 18:30:42 +0900

    [fish] Add recursive path finder on tab expansion
    This is mapped to ctrl-g, but the idea is similar to a tab-like
    completion. However, instead of completing from the local directory
    only, paths are completed recursively too.
    
    For example, if you have `file.c` in the current dir, then `a/file.c`
    and `b/c/file.c` and type `fil`, then you will only get the local dir on
    tab completion. However, ctrl-g will now present an fzf picker with all
    3 options.

--- fish/.config/fish/conf.d/keybinds.fish
+++ fish/.config/fish/conf.d/keybinds.fish
@@ -77,3 +77,9 @@ function __nextd-or-forward-char
     end
 end
 bind \e\[C __nextd-or-forward-char # right arrow
+
+# search recursively under the current dir for files by basename prefix and
+# present a path picker
+# e.g. by typing some.c, all the files under a/b/some.c, a/some.c and some.c
+# will be found, instead of Tab behaviour only seeing current dir's some.c
+bind \cg __recursive_path_pick
---
+++ fish/.config/fish/functions/__recursive_path_pick.fish
@@ -0,0 +1,94 @@
+function __recursive_path_pick --description "Recursive file finder"
+    # this picker depends on `fd` for recursive file discovery and `fzf`
+    # for the interactive selector
+    #
+    # if either command is missing, warn and stop here
+    if not command -q fd
+        printf "recursive path picker requires `fd`\n" >&2
+        return
+    end
+
+    if not command -q fzf
+        printf "recursive path picker requires `fzf`\n" >&2
+        return
+    end
+
+    # get the base command
+    #
+    # example: if the command line is `nvim file.c` and the cursor is at the
+    # end, this becomes `nvim`
+    #
+    # we use this to avoid running on the command name itself
+    set -l previous_tokens (commandline -xpc)
+
+    # get the token under the cursor
+    #
+    # example: if the command line is `nvim file.c`, this becomes `file.c`
+    set -l token (commandline -ct)
+
+    # if there is no previous token, then the cursor is still on the command
+    # name, so this binding should not try to replace it with a file path
+    if test (count $previous_tokens) -eq 0
+        commandline -f complete
+        return
+    end
+
+    # if there is no current token, fall back to normal fish completion
+    if test -z "$token"
+        commandline -f complete
+        return
+    end
+
+    # if the token starts with a dash, it is probably an option like `-h`
+    # or `--help`, so let fish handle it normally
+    if string match -qr -- '^-' $token
+        commandline -f complete
+        return
+    end
+
+    # if the user already typed a slash, they are already completing a path
+    # directly, so let fish handle it normally
+    if string match -q -- '*/*' $token
+        commandline -f complete
+        return
+    end
+
+    # collect recursive matches under the current directory
+    #
+    # `fd --glob "$token*"` does the recursive basename-prefix match directly,
+    # so with a token like `some.c` it will return paths such as:
+    #   some.c
+    #   a/b/some.c
+    #   a/c/some.c
+    #
+    # `--strip-cwd-prefix=always` removes the leading `./` from the results
+    set -l matches (
+        fd --hidden --exclude .git --type f --strip-cwd-prefix=always --glob "*$token*"
+    )
+
+    # if nothing matched, fall back to normal fish completion
+    if test (count $matches) -eq 0
+        commandline -f complete
+        return
+    end
+
+    # pass the matches into fzf
+    #
+    # `--select-1` means:
+    # if there is exactly one match, choose it automatically
+    #
+    # `--exit-0` means:
+    # return success even if there was only one match and no interactive
+    # selection step was needed
+    set -l picked (
+        printf '%s\n' $matches |
+        fzf --select-1 --exit-0 --prompt="path> "
+    )
+
+    # if you selected something, replace the current token with that path
+    if test -n "$picked"
+        commandline --current-token --replace -- $picked
+    end
+
+    commandline -f repaint
+end
